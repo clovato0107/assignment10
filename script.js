338d2a4b2b1a317e3fb5625894cad78871ec49fc
clovato0107 <chrystallovato0107@gmail.com>
2024-09-05 13:39:29 -0700

    removing node modules

--- script.js
+++ script.js
@@ -3,42 +3,39 @@
 // A Bootstrap styled table representing your choice of data.
 // A Bootstrap styled form that allows a user to add a new row to the table when clicking on submit.
 
+const formButton = document.getElementById("formSubmit");
 
-const formButton = document.getElementById('formSubmit')
+let number = 0;
+formButton.addEventListener("click", (event) => {
+  event.preventDefault();
 
-let number = 0
-formButton.addEventListener('click', (event) => {
-    event.preventDefault()
+  let firstName = document.getElementById("firstName").value;
+  let lastName = document.getElementById("lastName").value;
+  let email = document.getElementById("email").value;
 
+  letnewTableRow = document.createElement("tr");
+  // nodes
+  let numberNode = document.createElement("td");
+  numberNode.innerHTML = number;
+  newTableRow.append(numberNode);
 
-    let firstName = document.getElementById('firstName').value
-    let lastName = document.getElementById('lastName').value
-    let email = document.getElementById('email').value
+  let firstNameNode = document.createElement("td");
+  firstNameNode.innerHTML = firstName;
+  newTableRow.append(firstNameNode);
+  // appends the table
 
-    letnewTableRow = document.createElement('tr')
-    // nodes 
-    let numberNode = document.createElement('td')
-        numberNode.innerHTML = number
-        newTableRow.append(numberNode) 
+  let lastNameNode = document.createElement("td");
+  lastNameNode.innerHTML = lastName;
+  newTableRow.append(lastNameNode);
 
-    let firstNameNode = document.createElement('td')
-    firstNameNode.innerHTML = firstName
-    newTableRow.append(firstNameNode)
-    // appends the table
+  let emailNode = document.createElement("td");
+  emailNode.innerHTML = email;
+  newTableRow.append(emailNode);
 
-    let lastNameNode = document.createElement('td')
-    lastNameNode.innerHTML = lastName
-    newTableRow.append(lastNameNode)
+  document.getElementById("tBody").appendChild(newTableRow);
 
-    let emailNode= document.createElement('td')
-    emailNode.innerHTML = email
-    newTableRow.append(emailNode)
-
-
-    document.getElementById('tBody').appendChild(newTableRow)
-
-    document.getElementById('firstName').value = ''
-    document.getElementById('lastName').value = ''
-    document.getElementById('email').value = ''
-    number++
-})
+  document.getElementById("firstName").value = "";
+  document.getElementById("lastName").value = "";
+  document.getElementById("email").value = "";
+  number++;
+});
